abd7e18fecfac8378a4a952ea730e460d28cd9b0
Frelin Ampilahy <frelin.ampilahy@cpexterne.org>
2020-07-14 23:14:12 +0200

    add package-info files

---
+++ src/main/java/org/review/amz/design_pattern/creational/factory/package-info.java
@@ -0,0 +1,7 @@
+package org.review.amz.design_pattern.creational.factory;
+
+/**
+ * FACTORY
+ * used to define a runtime interface for creating an object.
+ * **/
+
---
+++ src/main/java/org/review/amz/design_pattern/creational/singleton/package-info.java
@@ -0,0 +1,7 @@
+package org.review.amz.design_pattern.creational.singleton;
+
+/**
+ * SINGLETON
+ * It is used to provide global point of access to the object.
+ * Singleton patterns are used in logging, caches, thread pools, configuration settings, device driver objects.
+ * **/
