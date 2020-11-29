0cefba00db4af4609c01d172194f213296d71472
Frelin Ampilahy <frelin.ampilahy@cpexterne.org>
2020-11-29 11:24:18 +0100

    clean code

--- src/main/java/org/review/search/engine/DefaultSearchEngine.java
+++ src/main/java/org/review/search/engine/DefaultSearchEngine.java
@@ -4,29 +4,35 @@ import java.util.*;
 
 public class DefaultSearchEngine extends AbstractSearchEngine {
 
-    public DefaultSearchEngine(Collection<String> fileContent, Collection<String> wordsToSearch) {
-        this.fileContent = fileContent;
-        this.wordsToSearch = wordsToSearch;
-    }
+  public DefaultSearchEngine(Collection<String> fileContent, Collection<String> wordsToSearch) {
+    this.fileContent = fileContent;
+    this.wordsToSearch = wordsToSearch;
+  }
 
-    @Override
-    public Integer processSearch() {
-        Set<String> newWordsToSearch = (Set<String>) wordsToSearch;
-        Set<String> newFileContent = (Set<String>) fileContent;
+  @Override
+  public Integer processSearch() {
+    Set<String> newWordsToSearch = (Set<String>) wordsToSearch;
+    Set<String> newFileContent = (Set<String>) fileContent;
 
-        if (!fileContent.isEmpty()) {
-            Iterator<String> wordsToSearchIterator = newWordsToSearch.iterator();
-            while (wordsToSearchIterator.hasNext()) {
-                String s = wordsToSearchIterator.next();
-                for (String str : newFileContent) {
-                    if (str.equals(s)) {
-                        wordsToSearchIterator.remove();
-                    }
-                }
-            }
-            return (int) ((float) (wordsToSearch.size() - newWordsToSearch.size()) / Float.valueOf(newWordsToSearch.size()) * 100);
-        } else {
-            return 0;
+    if (!fileContent.isEmpty()) {
+      List<String> wordsToSearchList =
+          new ArrayList<>(
+              Arrays.asList(newWordsToSearch.toArray(new String[newWordsToSearch.size()])));
+      Iterator<String> wordsToSearchIterator = wordsToSearchList.listIterator();
+      while (wordsToSearchIterator.hasNext()) {
+        String s = wordsToSearchIterator.next();
+        for (String str : newFileContent) {
+          if (str.equals(s)) {
+            wordsToSearchIterator.remove();
+          }
         }
+      }
+      return (int)
+          ((float) (newWordsToSearch.size() - wordsToSearchList.size())
+              / Float.valueOf(newWordsToSearch.size())
+              * 100);
+    } else {
+      return 0;
     }
+  }
 }
