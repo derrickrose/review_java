6a53081b913031100e03c42778661930d8d2ab63
Frelin Ampilahy <frelin.ampilahy@cpexterne.org>
2021-03-13 11:25:46 +0100

    fix zero of function

--- src/main/java/org/review/amz/algo/zero_of_function/ZeroOfFunction.java
+++ src/main/java/org/review/amz/algo/zero_of_function/ZeroOfFunction.java
@@ -2,28 +2,37 @@ package org.review.amz.algo.zero_of_function;
 
 import java.util.function.Function;
 
+import static java.lang.Math.abs;
+
 public class ZeroOfFunction {
 
-
-    public static final double[] doSearchRange(Function<Double, Double> function, double min, double max, double errorMargin) {
-        if (min > max) {
-            return null;
-        }
-        /*
-        if (function.apply(min) * function.apply(max) > 0) {
-            return null;
-        }*/
-
-        if (max - min < errorMargin) {
-            return new double[]{min, max};
-        }
-
-        double middle = min + max / 2;
-
-        if (function.apply(middle) - function.apply(min) < 0) {
-            return doSearchRange(function, min, middle, errorMargin);
-        } else {
-            return doSearchRange(function, middle, max, errorMargin);
-        }
+  public static final double[] doSearchRange(
+      Function<Double, Double> function, double min, double max, double errorMargin) {
+    if (min > max) {
+      return null;
     }
+
+    double middle = min + ((max - min) / 2d);
+
+    if (function.apply(middle).doubleValue() == 0d) {
+      return new double[] {middle, middle + errorMargin};
+    }
+
+    if (function.apply(max).doubleValue() * function.apply(min).doubleValue() < 0d
+        && abs((function.apply(max).doubleValue() - function.apply(min).doubleValue()))
+            <= errorMargin) {
+      return new double[] {min, max};
+    }
+
+    if (function.apply(max).doubleValue() * function.apply(min).doubleValue() < 0d
+        && abs(max - min) <= errorMargin) {
+      return new double[] {min, max};
+    }
+
+    if (function.apply(middle) * function.apply(min) < 0d) {
+      return doSearchRange(function, min, middle , errorMargin);
+    } else {
+      return doSearchRange(function, middle , max, errorMargin);
+    }
+  }
 }
--- src/test/java/org/review/amz/algo/zero_of_function/ZeroOfFunctionTest.java
+++ src/test/java/org/review/amz/algo/zero_of_function/ZeroOfFunctionTest.java
@@ -8,16 +8,16 @@ import static org.review.amz.algo.zero_of_function.ZeroOfFunction.doSearchRange;
 
 public class ZeroOfFunctionTest {
 
-    @Test
-    public void searchRange() {
+  @Test
+  public void searchRange() {
 
-        Function<Double, Double> function = (x) -> Math.pow(x, 2) - 4;
+    // Function<Double, Double> function = (x) -> Math.pow(x, 2) - 4;
+    Function<Double, Double> function = (x) -> x - 2d;
 
-        double[] range = doSearchRange(function, -1, 6, 0.01);
+    double[] range = doSearchRange(function, -10d, 10d, 0.01);
 
-        int a = 0;
+    System.out.println(range[0] + " - " + range[1]);
 
-
-
-    }
+    int a = 0;
+  }
 }
