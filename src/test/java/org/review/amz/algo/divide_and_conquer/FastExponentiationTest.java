e1341eabfdeb95a34b5dada2e95fc676095aa8c1
AMPILAHY Frelin (Canal Plus Prestataire) <frelin.ampilahy@cpexterne.org>
2021-03-15 19:00:06 +0100

    update code

--- src/test/java/org/review/amz/algo/divide_and_conquer/FastExponentiationTest.java
+++ src/test/java/org/review/amz/algo/divide_and_conquer/FastExponentiationTest.java
@@ -11,13 +11,14 @@ public class FastExponentiationTest {
         Assert.assertTrue(8 * 8 * 8 * 8 * 8 == FastExponentiation.calculateFastExponentiation(8, 5));
 
         long t1 = System.currentTimeMillis();
-        double a = 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8 * 8;
+        double a = 8 * 8 * 8 * 8 * 8;
         long t2 = System.currentTimeMillis();
         System.out.println("t2 - t1 " + (t2 - t1));
         long t3 = System.currentTimeMillis();
-        double c = FastExponentiation.calculateFastExponentiation(8, 32);
+        double c = FastExponentiation.calculateFastExponentiation(8, 5);
         long t4 = System.currentTimeMillis();
         System.out.println("t4 - t3 " + (t4 - t3));
+        System.out.println("a - c " + a + " " + c);
         //note that the function's execution time right here is a little bit higher but with bigger value of n, it will make diff
     }
 }
